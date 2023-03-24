5a3fc043d96a74d5ebafcb73bbd5bcf0f04d879c
VasilisPng <110207897+VasilisPng@users.noreply.github.com>
2023-03-24 21:46:52 +0200

    Add files via upload

--- UpdateSoftwares.bat
+++ UpdateSoftwares.bat
@@ -10,27 +10,17 @@ if %errorLevel% == 0 (
     exit /b 1
 )
 
-REM Check for updates using Winget
-echo Checking for updates using Winget...
-winget outdated > winget_updates.txt
-
-REM Check for updates using Chocolatey
-echo Checking for updates using Chocolatey...
-powershell -Command "Start-Process cmd -ArgumentList '/c choco outdated --source=https://chocolatey.org/api/v2/ > choco_updates.txt' -Verb RunAs"
-
-REM Set counter variable to 0
-set /a count=0
-
-REM Increment count for each package that needs to be updated in Winget
-for /f "tokens=*" %%i in (winget_updates.txt) do (
-    set /a count+=1
-)
-
-REM If choco_updates.txt exists, increment count for each package that needs to be updated in Chocolatey
-if exist choco_updates.txt (
-    for /f "tokens=*" %%i in (choco_updates.txt) do (
+REM Check for updates using Winget and Chocolatey
+for %%i in (winget choco) do (
+    echo Checking for updates from %%i...
+    %%i outdated > "%%i_updates.txt"
+    REM Increment count for each package that needs to be updated
+    for /f "tokens=*" %%j in ("%%i_updates.txt") do (
         set /a count+=1
     )
+    REM Upgrade all packages
+    %%i upgrade --all || echo Failed to update with %%i.
+    del "%%i_updates.txt"
 )
 
 REM Check if any updates are available
@@ -38,17 +28,8 @@ if %count%==0 (
     echo No updates available.
 ) else (
     echo %count% updates available. Updating...
-
-    REM Upgrade all packages with Winget
-    winget upgrade --all || echo Failed to update with Winget.
-
-    REM Upgrade all packages with Chocolatey
-    powershell -Command "Start-Process cmd -ArgumentList '/c choco upgrade all --source=https://chocolatey.org/api/v2/' -Verb RunAs" || echo Failed to update with Chocolatey.
 )
 
 REM Clean up temporary files
-del winget_updates.txt
-del choco_updates.txt
-
 echo Script complete. Exiting in 3 seconds...
 timeout 3
