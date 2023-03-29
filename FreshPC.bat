c93a8f5100c1553e601ff5bc1dcdf1a7d325b3f9
VasilisPng <110207897+VasilisPng@users.noreply.github.com>
2023-03-29 07:54:47 +0300

    Add files via upload

--- FreshPC.bat
+++ FreshPC.bat
@@ -1,5 +1,12 @@
 @echo off
 
+REM Set frequently used paths as variables
+set "temp_folder=%USERPROFILE%\AppData\Local\Temp"
+set "emptyStandbyList=%temp%\emptyStandbyList.bat"
+set "flushStandbyList=%temp%\flushStandbyList.bat"
+set "emptyCombinedPageList=%temp%\emptyCombinedPageList.bat"
+set "emptyModifiedPageList=%temp%\emptyModifiedPageList.bat"
+
 REM Check for administrator rights
 NET FILE >NUL 2>&1
 IF NOT "%ERRORLEVEL%" == "0" (
@@ -12,32 +19,67 @@ echo Cleaning temporary files...
 echo.
 
 :: Remove temporary files and folders from the current user's profile
-del /f /s /q "%USERPROFILE%\AppData\Local\Temp\*.*"
-RD /s /q "%USERPROFILE%\AppData\Local\Temp\"
+del /f /s /q "%temp_folder%\*.*"
+RD /s /q "%temp_folder%\"
 
 :: Remove temporary files and folders from all user profiles
-for /d %%a in ("%SystemDrive%\Users\*") do (
+pushd "%SystemDrive%\Users"
+for /d %%a in (*) do (
     del /f /s /q "%%a\AppData\Local\Temp\*.*"
     RD /s /q "%%a\AppData\Local\Temp\"
 )
+popd
 
 echo.
 echo Temporary files and folders have been cleaned.
 
-REM Clear standby list
+REM Clear standby list, combined page list, and modified page list
 echo.
-echo Clearing standby list...
+echo Clearing standby list, combined page list, modified page list, and working set of all processes...
+
+:: Purge standby list, combined page list, and modified page list
 typeperf "\Memory\Standby Cache Reserve Bytes" -sc 1 | findstr /r "[0-9]" > nul
 if %errorlevel% EQU 0 (
-    echo Purging standby list...
-    echo. > "%temp%\emptyStandbyList.txt"
-    type "%temp%\emptyStandbyList.txt" > "%temp%\emptyStandbyList.bat"
-    start /min "%temp%\emptyStandbyList.bat"
-    del /f /q "%temp%\emptyStandbyList.bat"
+    echo Purging standby list, combined page list, and modified page list...
+    echo. > "%emptyStandbyList%"
+    type "%emptyStandbyList%" > "%emptyStandbyList%"
+    start /min "%emptyStandbyList%"
+    del /f /q "%emptyStandbyList%"
+
+    echo.
+    echo Flushing pages from the lowest-priority Standby list to the Free list...
+    echo. > "%flushStandbyList%"
+    type "%flushStandbyList%" > "%flushStandbyList%"
+    start /low "%flushStandbyList%"
+    del /f /q "%flushStandbyList%"
+
+    echo.
+    echo Clearing combined page list...
+    echo. > "%emptyCombinedPageList%"
+    type "%emptyCombinedPageList%" > "%emptyCombinedPageList%"
+    start /min "%emptyCombinedPageList%"
+    del /f /q "%emptyCombinedPageList%"
+
+    echo.
+    echo Clearing modified page list...
+    echo. > "%emptyModifiedPageList%"
+    type "%emptyModifiedPageList%" > "%emptyModifiedPageList%"
+    start /min "%emptyModifiedPageList%"
+    del /f /q "%emptyModifiedPageList%"
+
+    echo.
+    echo Clearing working set of all processes...
+    for /f "skip=1" %%a in ('wmic process get processid') do wmic process where "processid=%%a" CALL EmptyWorkingSet
+
+    echo.
+    echo Clearing system working set...
+    wmic os set ClearPageFileAtShutdown=True > nul
+
 ) else (
     echo Standby list is empty.
 )
 
 echo.
-echo Standby list cleared.
+echo Standby list, combined page list, modified page list, and working set of all processes cleared.
+echo System working set cleared.
 timeout /t 3 > nul
