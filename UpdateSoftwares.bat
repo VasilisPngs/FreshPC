13c4c3fb16021c276f35004f8af0210dc963754c
VasilisPng <110207897+VasilisPng@users.noreply.github.com>
2023-04-26 15:57:42 +0300

    Add files via upload

--- UpdateSoftwares.bat
+++ UpdateSoftwares.bat
@@ -1,5 +1,8 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
+
+REM Set log file path
+set LOGFILE=%~dp0\result.log
 
 REM Check if running with elevated privileges
 net session >nul 2>&1
@@ -7,7 +10,24 @@ if %errorLevel% == 0 (
     echo Running with elevated privileges.
 ) else (
     echo Script requires elevated privileges to run. Please run as an administrator.
+    echo Script aborted. >> %LOGFILE%
     exit /b 1
+)
+
+REM Check if PowerShell is installed
+where powershell >nul 2>&1
+if %errorLevel% == 0 (
+    echo PowerShell is already installed.
+) else (
+    echo PowerShell is not installed. Installing...
+    powershell.exe -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr -useb https://aka.ms/install-powershell.ps1 | iex"
+    if %errorLevel% == 0 (
+        echo PowerShell installed successfully.
+    ) else (
+        echo Failed to install PowerShell.
+        echo Script aborted. >> %LOGFILE%
+        exit /b 1
+    )
 )
 
 REM Check if Winget is installed
@@ -21,7 +41,7 @@ if %errorLevel% == 0 (
         echo Winget installed successfully.
     ) else (
         echo Failed to install Winget.
-        exit /b 1
+        echo Failed to install Winget. >> %LOGFILE%
     )
 )
 
@@ -36,7 +56,7 @@ if %errorLevel% == 0 (
         echo Chocolatey installed successfully.
     ) else (
         echo Failed to install Chocolatey.
-        exit /b 1
+        echo Failed to install Chocolatey. >> %LOGFILE%
     )
 )
 
@@ -51,9 +71,15 @@ for %%i in (winget choco) do (
     )
     REM Upgrade all packages
     if "%%i"=="choco" (
-        %%i upgrade all -y || echo Failed to update with %%i.
+        %%i upgrade all -y || (
+            echo Failed to update with %%i.
+            echo Failed to update with %%i. >> %LOGFILE%
+        )
     ) else (
-        %%i upgrade --all || echo Failed to update with %%i.
+        %%i upgrade --all || (
+            echo Failed to update with %%i.
+            echo Failed to update with %%i. >> %LOGFILE%
+        )
     )
     del "%%i_updates.txt"
 )
@@ -61,8 +87,10 @@ for %%i in (winget choco) do (
 REM Check if any updates are available
 if %count%==0 (
     echo No updates available.
+    echo Script completed successfully. >> %LOGFILE%
 ) else (
     echo %count% updates available. Updating...
+    echo Script completed with updates. >> %LOGFILE%
 )
 
 REM Clean up temporary files
