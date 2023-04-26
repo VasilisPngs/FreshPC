63101762c2c3af24c097e38f9d0925f5953ea24e
VasilisPng <110207897+VasilisPng@users.noreply.github.com>
2023-04-26 16:14:10 +0300

    Add files via upload

--- UpdateSoftwares.bat
+++ UpdateSoftwares.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal EnableDelayedExpansion
+setlocal
 
 REM Check if running with elevated privileges
 net session >nul 2>&1
@@ -10,68 +10,17 @@ if %errorLevel% == 0 (
     exit /b 1
 )
 
-REM Check if PowerShell is installed
-where powershell >nul 2>&1
-if %errorLevel% == 0 (
-    echo PowerShell is already installed.
-) else (
-    echo PowerShell is not installed. Installing...
-    powershell.exe -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr -useb https://aka.ms/install-powershell.ps1 | iex"
-    if %errorLevel% == 0 (
-        echo PowerShell installed successfully.
-    ) else (
-        echo Failed to install PowerShell.
-        exit /b 1
-    )
-)
-
-REM Check if Winget is installed
-where winget >nul 2>&1
-if %errorLevel% == 0 (
-    echo Winget is already installed.
-) else (
-    echo Winget is not installed. Installing...
-    powershell.exe -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr -useb https://aka.ms/install-winget | iex"
-    if %errorLevel% == 0 (
-        echo Winget installed successfully.
-    ) else (
-        echo Failed to install Winget.
-    )
-)
-
-REM Check if Chocolatey is installed
-where choco >nul 2>&1
-if %errorLevel% == 0 (
-    echo Chocolatey is already installed.
-) else (
-    echo Chocolatey is not installed. Installing...
-    powershell.exe -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr -useb https://chocolatey.org/install.ps1 | iex"
-    if %errorLevel% == 0 (
-        echo Chocolatey installed successfully.
-    ) else (
-        echo Failed to install Chocolatey.
-    )
-)
-
 REM Check for updates using Winget and Chocolatey
-set count=0
 for %%i in (winget choco) do (
     echo Checking for updates from %%i...
-    %%i outdated > nul 2>&1
+    %%i outdated > "%%i_updates.txt"
     REM Increment count for each package that needs to be updated
-    if %errorLevel% == 0 (
+    for /f "tokens=*" %%j in ("%%i_updates.txt") do (
         set /a count+=1
-        REM Upgrade all packages
-        if "%%i"=="choco" (
-            %%i upgrade all -y > nul 2>&1 || (
-                echo Failed to update with %%i.
-            )
-        ) else (
-            %%i upgrade --all > nul 2>&1 || (
-                echo Failed to update with %%i.
-            )
-        )
     )
+    REM Upgrade all packages
+    %%i upgrade --all || echo Failed to update with %%i.
+    del "%%i_updates.txt"
 )
 
 REM Check if any updates are available
